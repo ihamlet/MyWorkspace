707975ae8241e984d9e91f836c1ae3b56d0f224e
ihamlet <369774533@qq.com>
2023-02-24 20:28:59 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你要部署在 https://<USERNAME>.github.io
-git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:<USERNAME>/<REPO>.git main
 
 cd -
